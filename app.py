a2695c11f3ede4c09114b4c45a3ecfa52b4af096
SaniyaBhagane <120231995+SaniyaBhagane@users.noreply.github.com>
2026-02-21 12:08:08 +0530

    new route

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello_worls():
     return '<p>Hello World !!</p>'
 
+@app.route('/hello')
+def hello():
+    return '<p> Hello Flask !!</p>'
+
 if __name__ == '__main__':
     app.run()
