d7ba963bbff1cd9cdb996d469b560dd39e3cd47d
SaniyaBhagane <120231995+SaniyaBhagane@users.noreply.github.com>
2026-02-21 13:02:08 +0530

    db integration

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
 from flask import Flask, render_template
-
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///databse.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    
+with app.app_context():
+    db.create_all()
+
+with app.app_context():
+    user = User(name= 'ABC', age=22, email= "abc@gmail.com")
+    db.session.add(user)
+    db.session.commit()
 
 @app.route('/')
 def hello_worls():
